3e771f5dc3fa4e478881b892624aa1c541b7167b
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 17:10:44 -0300

    resolvido, era uma troca de sinais que não pegava o meio da palavra se fosse impar

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,7 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word == "":
         return False
     
-    if low_index == high_index:
+    if low_index >= high_index:
         return True
     
     if word[low_index] != word[high_index]:
