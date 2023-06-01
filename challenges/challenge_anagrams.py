0e384436253d2e6d8df7da14221a0c0df99f26a2
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 20:17:00 -0300

    resolvido anagrams, mas ficou feio

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,18 @@
 def merge_sort(string):
     if len(string) <= 1:
         return string
-    
+
     mid = len(string) // 2
     left = merge_sort(string[:mid])
     right = merge_sort(string[mid:])
-    
+
     return merge(left, right)
+
 
 def merge(left, right):
     merged = []
     left_index, right_index = 0, 0
-    
+
     while left_index < len(left) and right_index < len(right):
         if left[left_index] <= right[right_index]:
             merged.append(left[left_index])
@@ -19,16 +20,16 @@ def merge(left, right):
         else:
             merged.append(right[right_index])
             right_index += 1
-    
+
     merged.extend(left[left_index:])
     merged.extend(right[right_index:])
-    
+
     return ''.join(merged)
 
 
 def is_anagram(first_string, second_string):
     if first_string == '' or second_string == '':
-        return (first_string, second_string, False)
+        return (merge_sort(first_string), merge_sort(second_string), False)
 
     first_string = first_string.lower()
     second_string = second_string.lower()
