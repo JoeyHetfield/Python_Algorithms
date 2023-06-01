c0377353961659d4aba525bd5816d4772795575d
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 17:44:50 -0300

    primeiro rascunho da anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,10 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    #  iniciando
-    raise NotImplementedError
+    if first_string == '' or second_string == '':
+        return False
+    
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    if len(first_string) != len(second_string):
+        return False
+    
