dbe08665fb266a71b48a367b3bb69d8ead6aa85a
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 15:45:09 -0300

    tentando diminuir complexidade do codigo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time:
-        return None
-   
     count = 0
 
     for period in permanence_period:
-        if None in period:
+        if None in period or not target_time:
             return None
-        
+
         start_time, end_time = period
         if isinstance(start_time, str) or isinstance(end_time, str):
             return None
-        
+
         if start_time <= target_time <= end_time:
             count += 1
-        
+
     return count
-
-
-
