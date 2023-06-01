1b44c2bce95bd93c80795a849854b3542a4992df
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 16:23:16 -0300

    primeiros passos req2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,9 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "Joey"
+    key = 3
+    assert encrypt_message(message, key) ==  "eJyo_"
+
