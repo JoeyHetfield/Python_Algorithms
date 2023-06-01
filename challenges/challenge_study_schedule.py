736a0900e6020d16cbddd8df20908b839cc5c28c
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 15:27:55 -0300

    requsiito 1 funcionando

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,20 @@
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
-    
+   
     count = 0
+
     for period in permanence_period:
-        if period[0] <= target_time <= period[1]:
+        if None in period:
+            return None
+        
+        start_time, end_time = period
+        if isinstance(start_time, str) or isinstance(end_time, str):
+            return None
+        
+        if start_time <= target_time <= end_time:
             count += 1
+        
     return count
 
 
