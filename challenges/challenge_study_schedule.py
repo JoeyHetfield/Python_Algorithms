54931a64954a2c4f2b90ab90a38e1665d39c9059
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 14:48:25 -0300

    primeiro modelo da study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    
+    count = 0
+    for period in permanence_period:
+        if period[0] <= target_time <= period[1]:
+            count += 1
+    return count
+
+
+
