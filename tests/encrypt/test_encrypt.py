b47978220b28a1dd1994ef3b9f575a551743918b
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 16:35:43 -0300

    aparentemente foi o req2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,7 +3,28 @@ import pytest
 
 
 def test_encrypt_message():
+    # caso os valores sejam errados, chave ou mensagem
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+
+    with pytest.raises(TypeError):
+        encrypt_message("Joey", "3")
+
+    # Se key não for um índice válido, retorna a string message invertida
+
+    message = "Joey"
+    key = 7
+    assert encrypt_message(message, key) == "yeoJ"
+
+    # Se key for um índice valido e par
+
+    message = "Joey"
+    key = 2
+    assert encrypt_message(message, key) == "ye_oJ"
+
+    # Se key for um índice valido e ímpar
+
     message = "Joey"
     key = 3
-    assert encrypt_message(message, key) ==  "eJyo_"
-
+    assert encrypt_message(message, key) == "eoJ_y"
