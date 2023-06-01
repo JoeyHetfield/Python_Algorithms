7bb48e4c57a312f2ead8d313fbf56ab69be01ca0
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-01 19:50:50 -0300

    algumas mudanças na função do course, e mudanças na is_anagram pra ficar de acordo, mas ainda não está tudo correto

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,34 +1,29 @@
-def merge_sort(numbers, start=0, end=None):
-    if end is None:
-        end = len(numbers)
-    if (end - start) > 1:  # se não reduzi o suficiente, continua
-        mid = (start + end) // 2  # encontrando o meio
-        merge_sort(numbers, start, mid)  # dividindo as listas
-        merge_sort(numbers, mid, end)
-        merge(numbers, start, mid, end)  # unindo as listas
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+    
+    mid = len(string) // 2
+    left = merge_sort(string[:mid])
+    right = merge_sort(string[mid:])
+    
+    return merge(left, right)
 
-# função auxiliar que realiza a mistura dos dois arrays
-
-
-def merge(numbers, start, mid, end):
-    left = numbers[start:mid]  # indexando a lista da esquerda
-    right = numbers[mid:end]  # indexando a lista da direita
-
-    left_index, right_index = 0, 0  # as duas listas começarão do início
-
-    for general_index in range(start, end):  # percorrer sobre a lista inteira
-        if left_index >= len(left):  # se os elementos da esquerda acabaram
-            numbers[general_index] = right[right_index]
-            right_index = right_index + 1
-        elif right_index >= len(right):  # se os elementos da direita acabaram
-            numbers[general_index] = left[left_index]
-            left_index = left_index + 1
-        elif left[left_index] < right[right_index]:
-            numbers[general_index] = left[left_index]
-            left_index = left_index + 1
+def merge(left, right):
+    merged = []
+    left_index, right_index = 0, 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
         else:
-            numbers[general_index] = right[right_index]
-            right_index = right_index + 1
+            merged.append(right[right_index])
+            right_index += 1
+    
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+    
+    return ''.join(merged)
 
 
 def is_anagram(first_string, second_string):
@@ -39,7 +34,15 @@ def is_anagram(first_string, second_string):
     second_string = second_string.lower()
 
     if len(first_string) != len(second_string):
-        return False
+        return (first_string, second_string, False)
 
     first_string = list(first_string)
     second_string = list(second_string)
+
+    first_string = merge_sort(first_string)
+    second_string = merge_sort(second_string)
+
+    sorted_first_string = "".join(first_string)
+    sorted_second_string = "".join(second_string)
+
+    return (sorted_first_string, sorted_second_string, first_string == second_string)
